02a12b863295260a6e23a3a0b5267f4b201eb2b5
Ryan Jones <ryandjones14@gmail.com>
2016-02-26 11:29:48 -0600

    finds 20 restaurants in city

--- food_finder.rb
+++ food_finder.rb
@@ -1,11 +1,54 @@
+# require 'pry'
+# require 'HTTParty'
+# require 'factual'
+# factual = Factual.new("qd8habB4vnjoRKFNn6UAaVh12xpdkskpV6AEy5wX", "WPYZFyL5yxICexWW3AtApl3zZpp1i5rs4AAHLKqX")
+
+# puts "Are u hungry? Yes or no?"
+# hungry = gets.strip.downcase
+
+# puts "What city are you in?"
+# city = gets.strip.capitalize
+
+# if hungry == "yes"
+#   def find_restaurant
+#     response = HTTParty.get("http://api.v3.factual.com/t/places-us?qfilters={city: #{city}}&KEY=qd8habB4vnjoRKFNn6UAaVh12xpdkskpV6AEy5wX")
+#     binding.pry
+#   end
+
+#   puts find_restaurant
+# else puts "scram."
+# end
+
+require 'httparty'
 require 'pry'
-require 'HTTParty'
 require 'factual'
 factual = Factual.new("qd8habB4vnjoRKFNn6UAaVh12xpdkskpV6AEy5wX", "WPYZFyL5yxICexWW3AtApl3zZpp1i5rs4AAHLKqX")
 
-def find_restaurant
-  response = HTTParty.get("http://api.v3.factual.com/t/places-us?filters={%22category_ids%22:{%22$includes%22:347}}&KEY=qd8habB4vnjoRKFNn6UAaVh12xpdkskpV6AEy5wX")
+#factual.table("places-us").schema
+
+
+
+
+
+puts "Welcome! Are you hungry? Yes or no?"
+  hungry = gets.strip.downcase
+if hungry == "yes"
+  puts "What city and state are you located in? EX: Austin,TX"
+  @location = gets.strip.capitalize
+
+def find_restaurant(location)
+  @location = location
+  response = HTTParty.get("http://api.v3.factual.com/t/restaurants-us?q=#{@location}&KEY=qd8habB4vnjoRKFNn6UAaVh12xpdkskpV6AEy5wX")
   binding.pry
 end
 
-puts find_restaurant
+
+  puts find_restaurant(@location)
+
+
+
+
+
+
+else puts "Scram!"
+end
