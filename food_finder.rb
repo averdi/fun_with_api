179130ec1f29bf9a3ae25c81493287dcdc28a798
Antonia Verdi <averdi@gm.slc.edu>
2016-02-26 13:28:33 -0600

    got it to display only 10 restaurants instead of 15

--- food_finder.rb
+++ food_finder.rb
@@ -20,13 +20,14 @@ factual = Factual.new("qd8habB4vnjoRKFNn6UAaVh12xpdkskpV6AEy5wX", "WPYZFyL5yxICe
        def find_restaurant(city, state)
       @city = city
       @state = state
-      response = HTTParty.get("http://api.v3.factual.com/t/restaurants-us?q=#{@city}, #{@state}&KEY=qd8habB4vnjoRKFNn6UAaVh12xpdkskpV6AEy5wX")
+      response = HTTParty.get("http://api.v3.factual.com/t/restaurants-us?q=#{@city}, #{@state}&limit=10&KEY=qd8habB4vnjoRKFNn6UAaVh12xpdkskpV6AEy5wX")
       restaurants_array = response["response"]["data"]
 
       restaurants_array.map do |rest_hash|
         name = rest_hash["name"]
         address = rest_hash["address"]
-        puts "Try #{name} located at #{address}"
+        # cuisine = rest_hash["cuisine"]
+        puts "Try #{name}, located at #{address}"
       end
 
 
@@ -34,6 +35,13 @@ factual = Factual.new("qd8habB4vnjoRKFNn6UAaVh12xpdkskpV6AEy5wX", "WPYZFyL5yxICe
 
 
       puts find_restaurant(@city, @state)
+
+#goals for after lunch:
+#display rest name, address, cuisine, alc yes/no
+#make it display nicely
+#make the restaurants display numerically
+#bonus: figure out how to get it to display top 10 by ranking
+#BONUS: loop to ask for different ones
 
 
 
