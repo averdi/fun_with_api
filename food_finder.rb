e27fd7962fcee9c37126ff21af7c50818ecdd90e
Antonia Verdi <averdi@gm.slc.edu>
2016-02-26 11:38:21 -0600

    when you prompt with a city and state it responds with 20 restaurants from that location

--- food_finder.rb
+++ food_finder.rb
@@ -1,3 +1,43 @@
 require 'httparty'
 require 'pry'
+require 'factual'
+factual = Factual.new("qd8habB4vnjoRKFNn6UAaVh12xpdkskpV6AEy5wX", "WPYZFyL5yxICexWW3AtApl3zZpp1i5rs4AAHLKqX")
+
+#factual.table("places-us").schema
+
+
+
+
+
+    puts "Welcome! Are you hungry? Yes or no?"
+      hungry = gets.strip.downcase
+    if hungry == "yes"
+         puts "What city are you in?"
+      @city = gets.strip.capitalize
+        puts "What state are you in?"
+      @state = gets.strip.upcase
+
+       def find_restaurant(city, state)
+      @city = city
+      @state = state
+      response = HTTParty.get("http://api.v3.factual.com/t/restaurants-us?q=#{@city}, #{@state}&KEY=qd8habB4vnjoRKFNn6UAaVh12xpdkskpV6AEy5wX")
+      binding.pry
+      end
+
+
+      puts find_restaurant(@city, @state)
+
+
+
+
+
+    else puts "Scram!"
+    end
+
+
+
+
+
+
+
 
